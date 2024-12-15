fc1091002b54bcb9932b6d59ff4da6cb07bae44e
Arian Chlebosz <chlebosz.adrian@gmail.com>
2024-12-15 23:53:54 +0100

    Handle errors that might appear during the message processing

--- src/main/kotlin/com/breader/MessageHandler.kt
+++ src/main/kotlin/com/breader/MessageHandler.kt
@@ -3,15 +3,19 @@ package com.breader
 import com.breader.command.CommandDispatcher
 import com.breader.protocol.MessageMarshaller
 import com.breader.protocol.MessageParser
+import com.breader.protocol.type.ErrorData
 
 class MessageHandler() {
     private val messageParser = MessageParser()
     private val commandDispatcher = CommandDispatcher()
     private val messageMarshaller = MessageMarshaller()
 
-    fun handle(message: String): String {
+    fun handle(message: String): String = runCatching {
         val parsedMessage = messageParser.parse(message)
         val result = commandDispatcher.dispatch(parsedMessage)
-        return messageMarshaller.marshall(result)
+        messageMarshaller.marshall(result)
+    }.getOrElse {
+        val error = ErrorData("ERR", it.message ?: "Unknown error")
+        messageMarshaller.marshall(error)
     }
 }
---
+++ src/test/kotlin/com/breader/MessageHandlerTest.kt
@@ -0,0 +1,19 @@
+package com.breader
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MessageHandlerTest {
+
+    private val messageHandler = MessageHandler()
+
+    @Test
+    fun `should handle message`() {
+        assertEquals("+PONG\r\n", messageHandler.handle("*1\r\n$4\r\nPING\r\n"))
+    }
+
+    @Test
+    fun `should handle handling error`() {
+        assertEquals("-ERR Unknown command\r\n", messageHandler.handle("*1\r\n$2\r\nHI\r\n"))
+    }
+}
